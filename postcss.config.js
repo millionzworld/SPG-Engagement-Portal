cb5ad841d264e3c1158d2856f0648a0490a18b57
millionzworld <michael@ssdm.co>
2025-11-11 10:07:04 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,3 @@ export default {
     autoprefixer: {},
   },
 }
-```
-
----
-
-## 7) tailwind.config.js
-```js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-}
