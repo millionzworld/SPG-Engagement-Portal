16f2e2c61c16f87d2823087fe15deea122c300e8
millionzworld <michael@ssdm.co>
2025-11-11 11:35:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/",            // ensure assets load from site root
+  base: "/", // keep this
 });
